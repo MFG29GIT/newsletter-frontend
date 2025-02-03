cddcc182ed4a2e243b3d0d2d05f619140b4ac370
Marco <marco.fancello@web.de>
2025-02-03 10:16:24 +0100

    project setup

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-teal-950">
+      <span className="animate-spin text-[14em]">🦭</span>
+    </div>
+  );
+}
+
